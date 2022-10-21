81603e25c91f22db3458124eb996d44677fbe914
hugo-ravailhe <hugo.rh@free.fr>
2022-10-21 14:35:21 +0200

    add new class

---
+++ PizzaController/PizzaController/Customer.cs
@@ -0,0 +1,22 @@
+﻿namespace PizzaController;
+
+public class Customer: Person
+{
+    private int _nextId = 1;
+    private int _id;
+    private int _phone;
+    private string _address;
+
+    public Customer(int phone, string address)
+    {
+        _id = _nextId;
+        this._address = address;
+        this._phone = phone;
+    }
+    
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Phone { get; set; }
+    public string Address { get; set; }
+
+}
---
+++ PizzaController/PizzaController/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace PizzaController;
+
+public class Person
+{
+    private string _firstname;
+    private string _lastname;
+
+    public Person()
+    {
+        
+    }
+    
+    public string Firstname { get; set; }
+    public string Lastname { get; set; }
+    
+}
